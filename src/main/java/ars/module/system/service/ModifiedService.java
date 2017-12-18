e587082ebb94ffe13d4fa1805dd4d76bc8653857
wuyq <314691309@qq.com>
2017-12-18 16:31:08 +0800

    Change the scope of method record to protected,and change the return type to void

--- src/main/java/ars/module/system/service/ModifiedService.java
+++ src/main/java/ars/module/system/service/ModifiedService.java
@@ -1,9 +1,6 @@
 package ars.module.system.service;
 
-import java.util.Map;
-
 import ars.invoke.local.Api;
-import ars.invoke.request.Requester;
 import ars.module.system.model.Modified;
 import ars.database.service.SearchService;
 
@@ -17,17 +14,5 @@ import ars.database.service.SearchService;
  */
 @Api("system/modified")
 public interface ModifiedService<T extends Modified> extends SearchService<T> {
-	/**
-	 * 记录删除数据
-	 * 
-	 * @param requester
-	 *            请求对象
-	 * @param entity
-	 *            对象实例
-	 * @param different
-	 *            差异属性值
-	 * @return 数据更新记录实例
-	 */
-	public T record(Requester requester, Object entity, Map<String, Object[]> different);
 
 }
